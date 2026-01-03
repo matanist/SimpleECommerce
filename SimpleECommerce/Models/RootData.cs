9ac83db9eae6afc35137b7d0baa1e13b3f716427
Fatih Baytar <matanist@hotmail.com>
2026-01-03 17:39:10 +0000

    Basit anlamda web api den verileri çekmek

---
+++ SimpleECommerce/Models/RootData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleECommerce.Models
+{
+    public class Data
+    {
+        public List<Product> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class RootData
+    {
+        public bool Success { get; set; }
+        public object Message { get; set; }
+        public Data Data { get; set; }
+        public object Errors { get; set; }
+    }
+}
--- SimpleECommerce/Views/MainPage.xaml.cs
+++ SimpleECommerce/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleECommerce.Views
+﻿using SimpleECommerce.Models;
+
+namespace SimpleECommerce.Views
 {
     public partial class MainPage : ContentPage
     {
@@ -9,6 +11,32 @@
             InitializeComponent();
         }
 
-        
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+#if ANDROID
+            client.BaseAddress = new Uri("http://10.0.2.2:5001/api/");
+#else
+            client.BaseAddress = new Uri("http://localhost:5001/api/");
+#endif
+            var response = await client.GetAsync("products?pageNumber=1&pageSize=1");
+            var contet = await response.Content.ReadAsStringAsync();
+            
+            var products = System.Text.Json.JsonSerializer.Deserialize<RootData>(contet, new System.Text.Json.JsonSerializerOptions
+            {
+                PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase,
+            });
+            var firstProduct = products?.Data?.Items?.FirstOrDefault();
+
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                lblName.Text = firstProduct?.Name;
+                lblPrice.Text = firstProduct != null ? $"${firstProduct.Price}" : string.Empty;
+                imgProduct.Source = firstProduct?.ImageUrl;
+            });
+
+
+            
+        }
     }
 }
